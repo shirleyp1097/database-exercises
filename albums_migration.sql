d89c4ff98ab7b8570902a1ddae00815942f075c9
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-10-01 15:35:16 -0500

    refactor

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums (
                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                       artist VARCHAR(65) NOT NULL,
-                       name VARCHAR(65) NOT NULL,
-                       release_date INT NOT NULL,
-                       sales FLOAT UNSIGNED,
-                       genre VARCHAR(65) NOT NULL,
+                       artist VARCHAR(128) NOT NULL,
+                       name VARCHAR(128) NOT NULL,
+                       release_date SMALLINT NOT NULL,
+                       sales FLOAT(6, 2),
+                       genre VARCHAR(128) NOT NULL,
                        PRIMARY KEY (id)
 );
