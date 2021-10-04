d6597ade4ad99ef424a3efd9e15b278f138f82d1
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-10-04 15:54:43 -0500

    finished exercise

--- update_exercises.sql
+++ update_exercises.sql
@@ -4,12 +4,12 @@ UPDATE albums
 SET sales = sales * 10;
 SELECT name as 'Name of album', sales as 'Sales' FROM albums;
 
-SELECT name as 'NAme', release_date as 'Release Date' FROM albums
+SELECT name as 'Name', release_date as 'Release Date' FROM albums
 WHERE release_date < 1980;
 UPDATE albums
 SET release_date = release_date -100
 WHERE release_date <= 1980;
-SELECT name as 'NAme', release_date as 'Release Date' FROM albums;
+SELECT name as 'Name', release_date as 'Release Date' FROM albums;
 
 
 SELECT name as 'Albums by Michael Jackson', artist as 'artist' FROM albums
