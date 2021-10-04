089e46f5362a547eca77335a9ede96107d37b382
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-10-04 16:19:02 -0500

    finished part 2

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,11 +1,18 @@
 USE employees;
 SELECT * FROM employees
-WHERE first_name = 'Irene'
+WHERE (first_name = 'Irene'
 OR first_name = 'Vidya'
-OR first_name = 'Maya';
+OR first_name = 'Maya')
+AND gender = 'male';
 
 SELECT * FROM employees
-WHERE last_name LIKE 'E%';
+WHERE last_name LIKE 'E%'
+OR last_name LIKE  '%E';
 
 SELECT * FROM employees
 WHERE last_name LIKE '%q%';
+
+SELECT * FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+;
