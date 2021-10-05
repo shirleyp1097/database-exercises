99aad811e593b9dc57fccecb75f52d753c7c527e
Paris Shirley <shirleyp1097@gmail.com>
2021-10-05 10:05:50 -0500

    work on where exercsies

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,17 +1,33 @@
 USE employees;
-SELECT * FROM employees
-WHERE (first_name = 'Irene'
-OR first_name = 'Vidya'
-OR first_name = 'Maya')
-AND gender = 'male';
+
+SHOW TABLES;
+
+DESCRIBE employees;
 
 SELECT * FROM employees
-WHERE last_name LIKE 'E%'
-OR last_name LIKE  '%E';
+WHERE first_name IN ('Irena','Vidya','Maya');
+
+SELECT * FROM employees
+WHERE last_name LIKE 'e%';
 
 SELECT * FROM employees
 WHERE last_name LIKE '%q%';
 
 SELECT * FROM employees
+WHERE gender = 'M'
+  AND
+    (first_name LIKE 'Irena'
+        OR first_name LIKE 'Vidya'
+        OR first_name LIKE 'Maya');
+
+SELECT * FROM employees
+WHERE last_name LIKE 'e%'
+   OR last_name LIKE '%e';
+
+SELECT * FROM employees
+WHERE last_name LIKE 'e%'
+  AND last_name LIKE '%e';
+
+SELECT * FROM employees
 WHERE last_name LIKE '%q%'
-AND last_name NOT LIKE '%qu%';
+  AND last_name NOT LIKE '%qu%';
